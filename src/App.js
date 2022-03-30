439dd4e3ac29c18ebf6c2cc898efeb58f87f870e
irenehl <26irenelopez@gmail.com>
2022-03-30 01:36:36 -0600

    feat: contact manager

--- src/App.js
+++ src/App.js
@@ -1,28 +1,103 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
 import './App.css';
+import api from '../src/api/contacts'
 import Header from './components/Header'
 import AddContact from './components/AddContact'
 import ContactList from './components/ContactList'
+import ContactDetail from './components/ContactDetail';
+import EditContact from './components/EditContact';
+import { BrowserRouter as Routes, Switch, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
-  const contacts = [
-    {
-      id: 1,
-      "name": "Daniela",
-      "email": "26irenelopez@gmail.com"
-    },
-    {
-      id: 2,
-      "name": "Irene",
-      "email": "26danielalopez@gmail.com"
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([])
+  const [searchTerm, setSearchTerm] = useState("")
+  const [searchResults, setSerchResults] = useState([])
+
+  //RetrieveContacts
+  const retrieveContacts = async () => {
+    const response = api.get("/contacts");
+
+    return response.data;
+  }
+
+  const addContactHandler = async (contact) => {
+    const request = {
+      id: nanoid(),
+      ...contact
     }
-  ];
-  
+
+    const response = await api.post("/contacts", request)
+
+    setContacts([...contacts, response.data])
+  }
+
+
+  const updateContactHandler = async (contact) => {
+    const response = await api.put(`/contacts/${contact.id}`, contact);
+    const { id, name, email } = response.data;
+
+    setContacts(contacts.map((contact) => {
+      return contact.id === id ? { ...response.data } : contact;
+    }));
+  };
+
+  const removeContactHandler = async (id) => {
+    await api.delete(`/contacts/${id}`);
+
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList)
+  }
+
+  const searchHandler = (searchTerm) => {
+    setSearchTerm(searchTerm);
+
+    if (searchTerm !== "") {
+      const newContactList = contacts.filter((contact) => {
+        return Object.values(contact)
+        .join(" ")
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase);
+      })
+      setSerchResults(newContactList)
+    } 
+    else {
+      setSerchResults(contacts)
+    }
+  };
+
+  useEffect(() => {
+    // const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    // retriveContacts ?? setContacts(retriveContacts);
+    const getAllContacts = async () => {
+      const allContacts = await retrieveContacts();
+      if (allContacts) setContacts(allContacts);
+    }
+
+    getAllContacts();
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts])
+
   return (
     <div className='ui container'>
-      <Header/>
-      <AddContact />
-      <ContactList contacts={contacts}/>
+      <BrowserRouter>
+        <Routes>
+          <Header />
+            <Route path="/" exact render={(props) => (<ContactList {...props} contacts={searchTerm.length < 1 ? contacts : searchResults} getContactId={removeContactHandler} term={searchTerm} searchKeyword={searchHandler} />)} />
+            <Route path="/add" render={(props) => (<AddContact {...props} addContactHandler={addContactHandler} />)} />
+            <Route path="/edit" render={(props) => (<EditContact {...props} updateContactHandler={updateContactHandler} />)} />
+            <Route path="/contact/id" component={ContactDetail} />
+          {/* <AddContact addContactHandler={addContactHandler}/> */}
+          {/* <ContactList contacts={contacts} getContactId={removeContactHandler}/> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/components/EditContact.js
+++ src/components/EditContact.js
@@ -1,28 +1,34 @@
 import React from 'react';
 
-class AddContact extends React.Component {
-    state = {
-        name: "",
-        email: "",
-    };
+class EditContact extends React.Component {
+    constructor(props) {
+        super(props) 
+        const { id, name, email } = props.location.state.contact;
+        this.state = {
+            id, 
+            name,
+            email,
+        };
+    }
 
-    add = (e) => {
+    update = (e) => {
         e.preventDefault();
         if(this.state.name === "" || this.state.email === "") {
             alert('All the fields are mandatory');
             return;
         }
 
-        this.props.addContactHandler(this.state);
+        this.props.updateContactHandler(this.state);
         this.setState({ name: "", email: "" });
-        this.props.history.push("/");  
+        this.props.history.push("/");
+        
     };
 
     render() {
         return (
             <div className='ui main'>
-                <h2>Add Contact</h2>
-                <form action="" className='ui form' onSubmit={this.add}>
+                <h2>Edit Contact</h2>
+                <form action="" className='ui form' onSubmit={this.update}>
                     <div className='field'>
                         <label htmlFor="">Name</label>
                         <input type="text" name='text' placeholder='Name' value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })}/>
@@ -31,11 +37,11 @@ class AddContact extends React.Component {
                         <label htmlFor="">Email</label>
                         <input type="text" name='text' placeholder='Email' value={this.state.email} onChange={(e) => this.setState({ email: e.target.value })}/>
                     </div>
-                    <button className='ui button blue'>Add</button>
+                    <button className='ui button blue'>Update</button>
                 </form>
             </div>
         );
     }
 }
 
-export default AddContact;
+export default EditContact;
